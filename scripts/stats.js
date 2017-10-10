83e620b1f0a9cc3b52757247c5225417a64e1bb8
Andrew Bloom <blooma116@gmail.com>
2017-10-09 17:28:44 -0700

    added conditionals regarding which message to display.

---
+++ scripts/stats.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// Get userName
+var userName = localStorage.userName;
+var currentUser = null;
+var playerObjectArray = JSON.parse(localStorage.playerObjectArray);
+
+for (var i = 0; i < playerObjectArray.length; i++){
+  if(playerObjectArray[i].name === userName){
+    currentUser = playerObjectArray[i];
+  }
+};
+
+// Relevant Object Properties
+// currentUser.name
+// currentUser.wins
+// currentUser.losses
+// currentUser.ties
+// currentUser.gamesPlayed
+
+function winPercentage(){
+  return (currentUser.wins / currentUser.gamesPlayed) * 100;
+}
+
+var welcome = document.getElementById('welcome');
+var losing = document.getElementById('losing');
+var winning = document.getElementById('winning');
+var trend = document.getElementById('trend');
+
+if (currentUser.gamesPlayed < 1) {
+  welcome.style.display = 'block';
+}
+
+if (winPercentage() <= 50){
+  losing.style.display = 'block';
+}
+
+if (winPercentage() > 50){
+  winning.style.display = 'block';
+}
+
+if (currentUser.gamesPlayed > 25){
+  trend.style.display = 'block';
+  losing.style.display = 'none';
+  winning.style.display = 'none';
+}
