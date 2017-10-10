04d5c8e7b78b03fe36ccbe5e6f6f081f5c61774b
Jeff Martinez <jeffxmartinez@gmail.com>
2017-10-10 13:55:17 -0700

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var learn = document.getElementById('learn');
 var paragraph = document.getElementById('paragraph');
 var rules = document.getElementById('rules');
@@ -11,40 +10,39 @@ var welcome = document.getElementById('welcome');
 var changeUser = document.getElementById('change-user');
 var play = document.getElementById('play');
 var scores = document.getElementById('scores');
+var signIn = document.getElementById('sign-in');
+var entry = document.getElementById('entry');
 
 if (localStorage.userName) {
-  console.log('sfsdf');
   var userName = localStorage.userName;
   doStuff();
 } else {
   userName = null;
 }
 
+entry.addEventListener('click', function(){
+  signIn.style.display = 'block';
+  list.style.display = 'none';
+  paragraph.style.display = 'none';
+});
+
 learn.addEventListener('click', function(){
-  if (paragraph.style.display == 'none') {
-    paragraph.style.display = 'block';
-    list.style.display = 'none';
-  } else {
-    paragraph.style.display = 'block';
-    list.style.display = 'none';
-  }
+  paragraph.style.display = 'block';
+  list.style.display = 'none';
+  signIn.style.display = 'none';
 });
 
 rules.addEventListener('click', function(){
-  if (list.style.display == 'none') {
-    list.style.display = 'block';
-    paragraph.style.display = 'none';
-  } else {
-    list.style.display = 'block';
-    paragraph.style.display = 'none';
-  }
+  list.style.display = 'block';
+  paragraph.style.display = 'none';
+  signIn.style.display = 'none';
 });
 
 function newElement(type, content, parent){
   var newEl = document.createElement(type);
   newEl.textContent = content;
   parent.appendChild(newEl);
-};
+}
 
 function handleChange() {
   login.style.display = 'block';
@@ -54,10 +52,7 @@ function handleChange() {
   welcome.innerHTML = null;
 }
 
-
-////when button is clicked, add username to local storage AND show Get Started button
-
-function handleLogin(e) {
+function handleLogin(e) { ////when button is clicked, add username to local storage AND show Get Started button
   e.preventDefault();
   userName = e.target.username.value;
   localStorage.userName = userName;
